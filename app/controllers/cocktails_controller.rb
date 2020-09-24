04badfb75e6d692bfad362917e77b5924e34cbd1
Benjamin Bruckner <benjamin.brueckner@bluewin.ch>
2020-09-24 16:31:02 +0200

    day finished

--- app/controllers/cocktails_controller.rb
+++ app/controllers/cocktails_controller.rb
@@ -36,6 +36,7 @@ class CocktailsController < ApplicationController
   def destroy
     @cocktail = Cocktail.find(params["id"])
     @cocktail.destroy
+    redirect_to cocktails_path
   end
 
   private
